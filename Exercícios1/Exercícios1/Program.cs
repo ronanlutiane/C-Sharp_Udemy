dcba18db693f76911552c1ef8c80d7659828abe3
ronanlutiane <47357259+ronanlutiane@users.noreply.github.com>
2019-03-21 18:53:47 -0300

    Exercicios Básicos
    Primeiros exercicios baseados no Curso Udemy

---
+++ Exercícios1/Exercícios1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercícios1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome completo");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Quantos Quartos tem na sua casa?");
+            int quartos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o valor de um produto");
+            double preco = double.Parse(Console.ReadLine());
+            Console.WriteLine("Informe seu ultimo nome, idade e altura (mesma linha)");
+            string[] vetor = Console.ReadLine().Split(' ');
+            string ultimoNome = vetor[0];
+            int idade = int.Parse(vetor[1]);
+            double altura = double.Parse(vetor[2]);
+            Console.WriteLine(nome);
+            Console.WriteLine(quartos);
+            Console.WriteLine(preco);
+            Console.WriteLine(ultimoNome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura);
+        }
+    }
+}
