6490acedf3af6d0ddbfa66345e7dc9d428a200cc
ronanlutiane <47357259+ronanlutiane@users.noreply.github.com>
2019-03-28 18:43:56 -0300

    Equação do 2º Grau
    Este código recebe valores de A, B e C do usuário e calcula raizes da equação do tipo Ax^2 + Bx + c = 0 usando o método de Bháskara.
    Falta tratamento de erros

---
+++ Equação do 2º Grau/Equação do 2º Grau/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Equação_do_2º_Grau
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        //Este codigo recebe os valores de A, B e C no input do usuário e calcula as raizes da Equação 
+        //do segundo grau baseado neste input
+        {
+            Console.WriteLine("Informe o valor de A: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o valor de B: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o valor de C: ");
+            int c = int.Parse(Console.ReadLine());
+            double delta = (b * b) - 4 * (a * c);
+            if (delta == 0)
+            {
+                double raiz = -b / (2 * a);
+                Console.WriteLine("Delta igual a 0, esta equação possui apenas uma raíz real: " + raiz);
+            }
+            else if (delta > 0)
+            {
+                double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                Console.WriteLine("Esta equação possui duas raízes reais: X1 = " + x1 + " e X2 = " + x2);
+            }
+            else
+            {
+                Console.WriteLine("Delta menor que 0, portanto esta equação não possui raízes reais.");
+            }
+        }
+    }
+}
