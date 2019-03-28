d129c16c5fea35342c283f5ad96d15398fa4d449
ronanlutiane <47357259+ronanlutiane@users.noreply.github.com>
2019-03-27 22:01:01 -0300

    Exercícios
    Exercícios basicos de calculos e prática em c#

---
+++ Exercício2/Exercício2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercício2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite um numero inteiro qualquer");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("digite um segundo inteiro qualquer");
+            int num2 = int.Parse(Console.ReadLine());
+            int soma = num1 + num2;
+            Console.WriteLine("Soma = " + Convert.ToString(soma));
+        }
+    }
+}
---
+++ Área do Círculo/Área do Círculo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Área_do_Círculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o raio da circunferência");
+            double raio = double.Parse(Console.ReadLine());
+            double area = Math.PI * (raio * raio);
+            Console.WriteLine("Área do círculo = " + Convert.ToString(area));
+        }
+    }
+}
