f0f731d6d4e9caa1a955e049ba8b894fdd01493a
ronanlutiane <47357259+ronanlutiane@users.noreply.github.com>
2022-02-15 18:27:38 -0300

    Voltando ao Git
    Commit de sincronizaçao. Estive muito tempo sem usar o GIt

--- Exercícios1/Exercícios1/obj/Release/netcoreapp2.2/Exercícios1.AssemblyInfo.cs
+++ Exercícios1/Exercícios1/obj/Release/netcoreapp2.2/Exercícios1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Área do Círculo")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Exercícios1")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Área do Círculo")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Área do Círculo")]
+[assembly: System.Reflection.AssemblyProductAttribute("Exercícios1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Exercícios1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
